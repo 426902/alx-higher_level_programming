7ea9cffd6e72fe4ce1ffd1c3c60262cb92c9d982
426902 <suleimanhabeeb16@gmail.com>
2022-09-22 01:54:15 -0700

    loop

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for c in str:
-        if ord(c) >= ord('a') and ord(c) <= ord('z'):
-            char = chr(ord(c) - 32)
-        else:
-            char = c
-        print("{:s}".format(char), end="")
-    print('')
-   # alp = list(str)
-    #for i in range(len(alp)):
-     #   if (ord(alp[i]) > 96 and ord(alp[i]) < 123):
-    #        alp[i] = chr(ord(alp[i]) - 32)
-     #   print("{}".format("".join(alp)))
+    alp = list(str)
+    for i in range(len(alp)):
+        if (ord(alp[i]) > 96 and ord(alp[i]) < 123):
+            alp[i] = chr(ord(alp[i]) - 32)
+    print("{}".format("".join(alp)))
