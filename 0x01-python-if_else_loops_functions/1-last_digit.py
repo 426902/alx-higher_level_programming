22950fc322625a81dbcb469669339ae0f8b4f42f
426902 <suleimanhabeeb16@gmail.com>
2022-09-22 01:41:08 -0700

    else if

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-digit = abs(number) % 10
+#digit = abs(number) % 10
 if number < 0:
-    digit = -digit
-    print("Last digit of {} is {} and is ".format(number, digit), end="")
-    if digit > 5:
-        print("greater than 5")
-    elif digit == 0:
-        print("0")
-    else:
-        print("less than 6 and not 0")
+    last = ((number * -1) % 10) * -1
+   #digit = -digit
+#print("Last digit of {} is {} and is ".format(number, digit), end="")
+else:
+    last = number % 10
+if digit > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number, last))
+   #print("greater than 5")
+elif last == 0:
+    print("Last digit of {} is {} and is 0".format(number, last))
+    #print("0")
+elif last < 6 and last != 0:
+    print("Last digit of {} is {} and is less\
+            than 6 and not 0".format(number, last))
+#else:
+ #   print("less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 def uppercase(str):
-    alp = list(str)
-    for i in range(len(alp)):
-        if (ord(alp[i]) > 96 and ord(alp[i]) < 123):
-            alp[i] = chr(ord(alp[i]) - 32)
-        print("{}".format("".join(alp)))
+    for c in str:
+        if ord(c) >= ord('a') and ord(c) <= ord('z'):
+            char = chr(ord(c) - 32)
+        else:
+            char = c
+        print("{:s}".format(char), end="")
+    print('')
+   # alp = list(str)
+    #for i in range(len(alp)):
+     #   if (ord(alp[i]) > 96 and ord(alp[i]) < 123):
+    #        alp[i] = chr(ord(alp[i]) - 32)
+     #   print("{}".format("".join(alp)))
